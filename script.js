10a7a190fe7733987a0f7b521cba7b04ee1acd9f
RizkyHerjuno <rizkyherjuno@gmail.com>
2025-02-24 22:44:40 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Toggle class active //
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika hamburger menu diklik //
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Toggle Class active untuk search form //
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Klik diluar sidebar untuk menghilangkan navbar //
+const hamburger = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("search-button");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+});
